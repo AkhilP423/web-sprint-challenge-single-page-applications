42f62e571ed8c55c91688728d9f232b9e1702f98
Akhil Pinnani <akhilp@Akhils-MacBook-Pro.local>
2021-11-12 15:12:40 -0800

    finished cypress testing

--- cypress/integration/orderTest.js
+++ cypress/integration/orderTest.js
@@ -10,7 +10,7 @@ describe('Order Test', () => {
         cy.visit('http://localhost:3000/Pizza')
     })
 
-    //test
+    //test defining
     const nameTest = () => cy.get('input[name="name"]').should('exist')
     const specialRequestTest = () => cy.get('input[name="specialRequest"]').should('exist')
     const pepperoniTest = () => cy.get('input[name="Pepperoni"]').should('exist')
@@ -21,4 +21,41 @@ describe('Order Test', () => {
     const meatballsTest = () => cy.get('input[name="Meatballs"]').should('exist')
     const buttonSubmitTest = () => cy.get("button").should('exist')
 
+    //testing existence of above units
+    it('Test Exists', () => {
+        //Start Assertion(s)
+        nameTest().should('exist')
+        specialRequestTest().should('exist')
+        pepperoniTest().should('exist')
+        hamTest().should('exist')
+        chickenTest().should('exist')
+        baconTest().should('exist')
+        sausageTest().should('exist')
+        meatballsTest().should('exist')
+        buttonSubmitTest().should('exist')
+    })
+    
+    //test for values being saved after typing in text boxes
+    it('Type Test', () => {
+        nameTest().type('Akhil')
+        nameTest().should('have.value', 'Akhil')
+        specialRequestTest().type('Please spit on my pizza.')
+        specialRequestTest().should('have.value', 'Please spit on my pizza.')
+    })
+
+    //test if checkboxes work
+    it('Check Checkboxes', () => {
+        pepperoniTest().check()
+        hamTest().check()
+        chickenTest().check()
+        baconTest().check()
+        sausageTest().check()
+        meatballsTest().check()
+    })
+
+    //submit form
+    it('Submit Form', () => {
+        buttonSubmitTest().click()
+    })
 })
+
